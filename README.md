da5e2bf5440b3cc1ce149530dabb005bcd17ffd7
Mustafa Bell <63203400+MooseStack@users.noreply.github.com>
2025-08-05 19:09:23 -0500

    add containerfile, download script, readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # container-packaged-ai-models
 AI Models stored in container images
+
+## From [Hugging Face](https://huggingface.co/models)
+
+1. [openai/gpt-oss-20b](https://huggingface.co/openai/gpt-oss-20b)
+   - Dockerhub: `docker.io/moosestack/packaged-model-openai_gpt-oss-20b:latest`
+   - Build Example: `podman build --build-arg HUGGINGFACE_MODEL_REPO=openai/gpt-oss-20b -t gpt-oss-20b:latest ./huggingface`
---
+++ huggingface/download_model.py
@@ -0,0 +1,17 @@
+import os
+from huggingface_hub import snapshot_download
+
+def main():
+    repo_id = os.getenv("HUGGINGFACE_MODEL_REPO")
+    print(f"Downloading model from Hugging Face repository: {repo_id}")
+    if not repo_id:
+        raise EnvironmentError("HUGGINGFACE_MODEL_REPO environment variable is not set.")
+
+    snapshot_download(
+        repo_id=repo_id,
+        local_dir="/models",
+        allow_patterns=["*.safetensors", "*.json", "*.txt"],
+    )
+
+if __name__ == "__main__":
+    main()
